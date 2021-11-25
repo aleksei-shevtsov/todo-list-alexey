07c43cb08f31ae541982994243c9709806214cd6
Nikolay Raskin <nikolay@DESKTOP-4NB49T3.localdomain>
2021-11-25 15:33:13 +0300

    Add strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e3806808f379f6beee6a1c6ee859c963'),
+    },
+  },
+});
