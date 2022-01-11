3d66542fd17a7065c519c8a8f96f8e87f4ad310c
Alex Shevtsov <shevtsov.alexey.dev@gmail.com>
2022-01-11 16:37:55 +0300

    syntax fix

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,37 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './app.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Context from '../../context';
 import LoginPage from '../login-page/login-page';
 import AppContent from '../app-content/app-content';
 
-// function setTokenToStorage(userToken) {
-//   return sessionStorage.setItem('token', userToken);
-// }
-
-// function getTokenFromStorage() {
-//   return sessionStorage.getItem('token');
-// }
-
 function App() {
 
-  // const [token, setToken] = useState(getTokenFromStorage())
-
-  // function handlerToken(newToken) {
-  //   setToken(newToken);
-  //   setTokenToStorage(newToken)
-  // }
-
   return (
-  <BrowserRouter>
-      <div className="wrapper">
-        <Routes>
-          <Route path="/" element={<LoginPage/>}/>
-          <Route path="/register" element={<LoginPage/>}/>
-          <Route path="/app" element={<AppContent/>}/>
-        </Routes>
-      </div>
-  </BrowserRouter>
+    <BrowserRouter>
+        <div className="wrapper">
+          <Routes>
+            <Route path="/" element={<LoginPage/>}/>
+            <Route path="/register" element={<LoginPage/>}/>
+            <Route path="/app" element={<AppContent/>}/>
+          </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -5,7 +5,8 @@ const Modal = ({active, setActive, children}) => {
 
     return (
         <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
-            <div className="modal__content" onClick={event => event.stopPropagation()}>
+            {/* stopPropagation makes that window dont close by click on content section. Only click on background closing window */}
+            <div className="modal__content" onClick={event => event.stopPropagation()}> 
                 {children}
             </div>
         </div>
